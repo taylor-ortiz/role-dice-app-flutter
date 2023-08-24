2cadf964057818497630512d77400a40d59dd6a6
taylor-ortiz <ortiz.taylor@gmail.com>
2023-08-24 06:41:37 -0600

    adding new class for Gradient Container and separating it out into its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:role_dice_app/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text('Hello World!!'),
-          ),
-        ),
-      ),
+    const MaterialApp(
+      home: Scaffold(body: GradientContainer()),
     ),
   );
 }
